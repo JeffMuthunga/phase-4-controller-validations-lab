62df8d700b0c6f1625a94c20d532e527bda64675
Jeff Muthunga <jeff.wainaina@student.moringaschool.com>
2023-03-17 00:12:09 +0300

    Complete

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::API
+
+rescue_from ActiveRecord::RecordInvalid, with: :render_custom_error
+
+
+private
+
+def render_custom_error(invalid)
+    render json: {errors: invalid.record.errors}, status: 422
+end
+
 end
